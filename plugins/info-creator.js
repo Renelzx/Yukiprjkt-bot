3f5a62aec2d87f5ba892fe00b7a8165f6c8c7409
Renelz <131983767+Renelzx@users.noreply.github.com>
2023-06-09 21:21:08 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; Zeltoria\nNICKNAME: Zeltoria\nORG: Zeltoria\nTITLE:\nitem1.TEL;waid=6285760451683:+62 857 6045 1683\nitem1.X-ABLabel: Nomor Owner\nitem2.URL:https://www.zeltoria.my.id\nitem2.EMAIL;type=INTERNET: elistz21@gmail.com\nitem2.X-ABLabel:\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; Renelz\nNICKNAME: Renelz\nORG: Renelz\nTITLE:\nitem1.TEL;waid=6287780999540:+62 877 8099 9540\nitem1.X-ABLabel: Nomor Owner\nitem2.URL:https://github.com/Renelzx\nitem2.EMAIL;type=INTERNET: renelzuwu@gmail.com\nitem2.X-ABLabel:\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
 m.reply(`Hay Kak Itu Ownerku, Jangan Macam~Macam Ya`)
 }
 if (command == 'creator') {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; Danil Elistz\nNICKNAME: Zeltoria\nORG: Zeltoria\nTITLE:\nitem1.TEL;waid=6285760451683:+62 857 6045 1683\nitem1.X-ABLabel: Nomor Owner\nitem2.URL:https://www.zeltoria.my.id\nitem2.EMAIL;type=INTERNET: elistz21@gmail.com\nitem2.X-ABLabel:\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; Renelz\nNICKNAME: Renelz\nORG: Renelz\nTITLE:\nitem1.TEL;waid=6287780999540:+62 877 8099 9540\nitem1.X-ABLabel: Nomor Owner\nitem2.URL:https://github.com/Renelzx\nitem2.EMAIL;type=INTERNET: renelzuwu@gmail.com\nitem2.X-ABLabel:\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
 m.reply(`Hai Kak Itu Nomor Developerku Yang Memprogram Aku, Mohon Jangan Di Spam Ya >,<`)
 }
