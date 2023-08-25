9d2f60167c759cdd972aa6ed74f304cce1b06d43
Renelz <131983767+Renelzx@users.noreply.github.com>
2023-08-25 21:45:50 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
 if (!args[0]) throw `Linknya?`
-  let res = await fetch(`https://api.itsrose.site/downloader/tiktok?url=${args[0]}&apikey=${global.rose}`)
+  let res = await fetch(`https://xzn.wtf/api/tiktok?url=${args[0]}&apikey=${global.xzn}`)
   let x = await res.json()
   let cap = `Nih Kak Videonya >,<`
-  conn.sendFile(m.chat, x.download.nowm, 'tiktok.mp4', cap, m)
+  conn.sendFile(m.chat, x.data.play, 'tiktok.mp4', cap, m)
 }
 handler.help = ['tiktok','ttdl']
 handler.tags = ['downloader']
